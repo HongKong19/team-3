8ad80bcecd1a084d51957ef7ce770a3dc271a8d1
Srijak Sengupta <45115715+srijaksengupta@users.noreply.github.com>
2019-10-18 14:07:55 +0800

    Pushing changes

--- cfg/urls.py
+++ cfg/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cfg/', include('cfg_team_3.urls')),
+    path('', include('cfg_team_3.urls'))
 ]
--- cfg_team_3/views.py
+++ cfg_team_3/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# def index(request):
+#     return HttpResponse('Hello')
+
+def index(request):
+    return render(request, 'cfg_team_3/index.html')
+
+
+# def processed(request):
+#     return HttpResponse('We are at processed function we created')
+
